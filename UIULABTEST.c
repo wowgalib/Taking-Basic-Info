562cdd639a108a33d8dc178735b330f7a245ad57
Abdullah Al Galib <132352002+wowgalib@users.noreply.github.com>
2023-10-01 20:44:19 +0600

    Update UIULABTEST.c

--- UIULABTEST.c
+++ UIULABTEST.c
@@ -1,4 +1,4 @@
-/* #include<stdio.h>
+#include<stdio.h>
 int main() {
     char Name;
     int ID;
@@ -9,22 +9,4 @@ int main() {
     scanf("%d", &ID);
     printf("Hey dear %s your ID is %d", Name, ID);
     return 0;
-} */
-
-#include<stdio.h>
-
-int main() {
-    char Name[50]; // Declare Name as a character array to store a name
-    int ID;
-
-    printf("Hello ICS! I'm taking your basic info.\n");
-    printf("Write your name:");
-    scanf("%s", Name); // Use a character array to store the name, no & needed
-    printf("Write your ID:");
-    scanf("%d", &ID); // Use & to read an integer
-
-    printf("Hey dear %s, your ID is %d\n", Name, ID); // Add a newline character
-
-    return 0;
 }
-
